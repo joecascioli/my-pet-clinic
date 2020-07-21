2de799c4891b8d63f7a353495905916110b28bcc
jcascioli <joeacascioli@gmail.com>
2020-07-21 14:58:34 -0400

    initial adding of POJOs. Closes #2.

---
+++ src/main/java/guru/springframework/mypetclinic/model/Owner.java
@@ -0,0 +1,4 @@
+package guru.springframework.mypetclinic.model;
+
+public class Owner extends Person{
+}
---
+++ src/main/java/guru/springframework/mypetclinic/model/Vet.java
@@ -0,0 +1,4 @@
+package guru.springframework.mypetclinic.model;
+
+public class Vet extends Person {
+}
