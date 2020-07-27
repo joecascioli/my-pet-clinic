db59cf8e1849f82ce1ced42d5500950288f03987
jcascioli <joeacascioli@gmail.com>
2020-07-27 14:08:47 -0400

    set up specialty map service closes #33

---
+++ pet-clinic-data/src/main/java/guru/springframework/mypetclinic/services/SpecialtyService.java
@@ -0,0 +1,6 @@
+package guru.springframework.mypetclinic.services;
+
+import guru.springframework.mypetclinic.model.Specialty;
+
+public interface SpecialtyService extends CrudService<Specialty, Long> {
+}
