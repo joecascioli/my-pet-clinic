bb108ab6d597a6bc63c88147d138130b6bd8df42
jcascioli <joeacascioli@gmail.com>
2020-07-29 15:07:28 -0400

    Implemented Owner JPA closes #38

--- pet-clinic-data/src/main/java/guru/springframework/mypetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/guru/springframework/mypetclinic/model/Pet.java
@@ -1,12 +1,24 @@
 package guru.springframework.mypetclinic.model;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
+@Entity
+@Table(name = "pets")
 public class Pet extends BaseEntity{
 
+    @Column(name = "name")
     private String name;
+
+    @Column(name = "birth_date")
     private LocalDate birthday;
+
+    @ManyToOne
+    @JoinColumn(name = "type_id")
     private PetType petType;
+
+    @ManyToOne
+    @JoinColumn(name = "owner_id")
     private Owner owner;
 
     public String getName() {
